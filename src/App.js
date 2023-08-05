455e1176df4e501304999e4fc0529ed91b0031da
mrzkhrmn <mrzkhrmn@hotmail.com>
2023-08-05 12:33:05 +0300

    productPage and ProductCard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { ProductPage } from "./pages/ProductPage";
 
 function App() {
   return (
     <ChakraProvider>
       <Router>
         <Navbar />
+        <main>
+          <Routes>
+            <Route path="/products" element={<ProductPage />}></Route>
+          </Routes>
+        </main>
       </Router>
     </ChakraProvider>
   );
---
+++ src/pages/ProductPage.jsx
@@ -0,0 +1,17 @@
+import { Center, Wrap, WrapItem } from "@chakra-ui/react";
+import { products } from "../products";
+import { ProductCard } from "../components/ProductCard";
+
+export const ProductPage = () => {
+  return (
+    <Wrap spacing="30px" justify="center" minHeight="100vh">
+      {products.map((product) => (
+        <WrapItem key={product._id}>
+          <Center w="250px" h="550px">
+            <ProductCard product={product} />
+          </Center>
+        </WrapItem>
+      ))}
+    </Wrap>
+  );
+};
